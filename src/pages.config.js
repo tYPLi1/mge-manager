fc92edf5b14c1780521f77bfa8a7e7177cb93d96
till <till@kastu.ch>
2026-03-10 11:29:30 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -62,6 +62,7 @@ import Punishments from './pages/Punishments';
 import Results from './pages/Results';
 import Rules from './pages/Rules';
 import Transactions from './pages/Transactions';
+import AdminEventConfig from './pages/AdminEventConfig';
 import __Layout from './Layout.jsx';
 
 
@@ -81,6 +82,7 @@ export const PAGES = {
     "Results": Results,
     "Rules": Rules,
     "Transactions": Transactions,
+    "AdminEventConfig": AdminEventConfig,
 }
 
 export const pagesConfig = {
