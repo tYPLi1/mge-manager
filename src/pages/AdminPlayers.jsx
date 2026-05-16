a3806585a76f8c181a80f62e7692448aa095192e
till <till@kastu.ch>
2026-05-16 21:10:13 +0000

    File changes

--- src/pages/AdminPlayers.jsx
+++ src/pages/AdminPlayers.jsx
@@ -55,7 +55,7 @@ export default function AdminPlayers() {
 
   const { data: players = [], isLoading } = useQuery({
     queryKey: ["players"],
-    queryFn: () => base44.entities.Player.list("name", 500),
+    queryFn: () => base44.entities.Player.list("name", 5000),
   });
 
   const { data: penalties = [] } = useQuery({
